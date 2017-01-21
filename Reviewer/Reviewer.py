63065864a5a487ef8ea3020cff963327fb508109
Tarek2 <tarek2@ubuntu.ubuntu-domain>
2017-01-21 01:09:08 +0100

    revrep

---
+++ Reviewer/Reviewer.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+def lev(source, target):
+    if len(source) < len(target):
+        return lev(target, source)
+
+    # So now we have len(source) >= len(target).
+    if len(target) == 0:
+        return len(source)
+
+    # We call tuple() to force strings to be used as sequences
+    # ('c', 'a', 't', 's') - numpy uses them as values by default.
+    source = np.array(tuple(source))
+    target = np.array(tuple(target))
+
+    # We use a dynamic programming algorithm, but with the
+    # added optimization that we only need the last two rows
+    # of the matrix.
+    previous_row = np.arange(target.size + 1)
+    for s in source:
+        # Insertion (target grows longer than source):
+        current_row = previous_row + 1
+
+        # Target and source items are aligned, and either
+        # are different (cost of 1), or are the same (cost of 0).
+        current_row[1:] = np.minimum(
+                current_row[1:],
+                np.add(previous_row[:-1], target != s))
+
+        # Deletion (target grows shorter than source):
+        current_row[1:] = np.minimum(
+                current_row[1:],
+                current_row[0:-1] + 1)
+
+        previous_row = current_row
+
+    return previous_row[-1]
+
+class Dictionary:
+    def __init__(self,words):
+        self.words=words
+        
+    def find_most_similar(self,term):
+      minimum=min([lev(term,c) for c in self.words])
+      return self.words[[minimum==lev(term,c) for c in self.words].index(True)]
+        

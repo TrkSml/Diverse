4344e04fef06f0834c870b5cd4534c25aee2a6c1
deepsml <tarek.samaali.92@gmail.com>
2017-01-21 01:13:32 +0100

    Update Reviewer.py

--- Reviewer/Reviewer.py
+++ Reviewer/Reviewer.py
@@ -1,33 +1,30 @@
+"""
+
+This is a Dictionary-based Analyzer/Reviewer based on Levenshtein distance. 
+All you have to do first is define a dictionary (list/array) . Any word you type in will be matched with the most approximate
+item contained within the dictionat .
+
+"""
 import numpy as np
 
 def lev(source, target):
     if len(source) < len(target):
         return lev(target, source)
 
-    # So now we have len(source) >= len(target).
     if len(target) == 0:
         return len(source)
 
-    # We call tuple() to force strings to be used as sequences
-    # ('c', 'a', 't', 's') - numpy uses them as values by default.
     source = np.array(tuple(source))
     target = np.array(tuple(target))
 
-    # We use a dynamic programming algorithm, but with the
-    # added optimization that we only need the last two rows
-    # of the matrix.
     previous_row = np.arange(target.size + 1)
     for s in source:
-        # Insertion (target grows longer than source):
         current_row = previous_row + 1
 
-        # Target and source items are aligned, and either
-        # are different (cost of 1), or are the same (cost of 0).
         current_row[1:] = np.minimum(
                 current_row[1:],
                 np.add(previous_row[:-1], target != s))
 
-        # Deletion (target grows shorter than source):
         current_row[1:] = np.minimum(
                 current_row[1:],
                 current_row[0:-1] + 1)

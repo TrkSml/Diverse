d0d36f5ceacc0a3e1449fdf5bb7e4f287a8d8e99
deepsml <tarek.samaali.92@gmail.com>
2017-02-07 23:03:53 +0100

    Update ChemicalElements.py

--- ChemicalElements/ChemicalElements.py
+++ ChemicalElements/ChemicalElements.py
@@ -49,7 +49,7 @@ class chemical_elements:
 	    """
 	    
 	     We will work with the elements' positions at first. For this purpose, the function gives back an array
-	     of the wanted position. A little difficulty here to combine single-word elements and double-word ones.
+	     of the wanted positions. A little difficulty here to combine single-word elements and double-word ones.
 	    
 	    """
 	
